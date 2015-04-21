03dbe248b91a2dea2910e7c1dafbc773eacb73b2
David Eliason & Brian Kropff <student@epicodus.com>
2015-04-21 11:53:30 -0700

    fix the darned bug in scrpt scr to script src

---
+++ controllers/FieldTripsController.js
@@ -0,0 +1,4 @@
+studentRoster.controller('FieldTripsCtrl', function FieldTripsCtrl($scope) {
+    $scope.studentsWithPermission = [{ name: "Jane Doe" }, { name: "John Doe"}];
+    $scope.test = [1,2,3];
+});
