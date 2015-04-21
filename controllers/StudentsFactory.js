f5adf77a0b44c9df45c361eaacc51043fc3cf499
David Eliason & Brian Kropff <student@epicodus.com>
2015-04-21 14:56:10 -0700

    Fix error, finish completed code

--- controllers/StudentsFactory.js
+++ controllers/StudentsFactory.js
@@ -1,16 +1,16 @@
 studentRoster.factory('StudentsFactory', function StudentsFactory() {
-        var factory = {};
-        factory.students = [];
+  var factory = {};
+  factory.students = [];
 
-        factory.addStudent = function() {
-            var student = { name: factory.studentName, permissionSlip: false };
-            factory.students.push(student);
-            factory.studentName = null;
-        };
+  factory.addStudent = function() {
+    var student = { name: factory.studentName, permissionSlip: false };
+    factory.students.push(student);
+    factory.studentName = null;
+  };
 
-        factory.deleteStudent = function(student) {
-            var index = factory.students.indexOf(student);
-            factory.students.splice(index, 1)
-        };
-        return factory;
+  factory.deleteStudent = function(student) {
+    var index = factory.students.indexOf(student);
+    factory.students.splice(index, 1);
+  };
+  return factory;
 });
